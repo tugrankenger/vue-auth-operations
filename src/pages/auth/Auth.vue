7f9090dd17684d6aa7bd5c6c6b508ba17ca43190
tugrankenger <turankenger@hotmail.com.tr>
2023-01-09 22:18:00 +0300

    create auth page

--- src/pages/auth/Auth.vue
+++ src/pages/auth/Auth.vue
@@ -1,3 +1,47 @@
 <template>
-  <!--Auth operations-->
+  <div class="container">
+    <div class="row my-5 justify-content-center">
+      <div class="col-6 card card-primary p-3 border" :class="{'border-primary': isUser, 'border-success': !isUser}">
+        <h3 class="text-center my-3" :class="{'text-primary':isUser, 'text-success': !isUser}">Vuejs Auth Operations</h3>
+        <hr>
+        <form @submit.prevent="onSubmit">
+          <div class="form-group">
+            <label>Email</label>
+            <input v-model="user.email" type="email" class="form-control" placeholder="Enter your email">
+          </div>
+          <div class="form-group mt-3">
+            <label>Password</label>
+            <input v-model="user.password" type="password" class="form-control" placeholder="Enter your password">
+          </div>
+          <div class="button-container d-flex flex-column align-items-center">
+            <button type="submit" class="btn btn-block my-2" :class="{'btn-primary':isUser, 'btn-success': !isUser}">
+              {{ isUser? 'Login': 'Register' }}
+            </button>
+            <a href="javascirpt:void(0)" @click.prevent="isUser = !isUser">
+              {{ isUser? 'I am not a member': 'I am a member' }}
+            </a>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+  export default{
+    data(){
+      return{
+        user:{
+          email:null,
+          password: null
+        },
+        isUser:false
+      }
+    },
+    methods:{
+      onSubmit(){
+        alert(this.user)
+      }
+    }
+  }
+</script>
